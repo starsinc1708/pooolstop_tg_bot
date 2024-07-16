fc713fb7dd57b4cfeedb4534071b5ea7c5dfb595
timatima <starsinc12@yandex.ru>
2024-07-16 10:20:49 +0400

    update db_utils

--- db_utils.py
+++ db_utils.py
@@ -10,13 +10,13 @@ db = client['pooolstop_webapp']
 user_from_web_app = db['user_web_app']
 user_collection = db['users']
 
-def user_sync(user_id, email: str):
+async def user_sync(user_id, email: str):
     user_collection.update_one({'user_id': user_id}, {'$set': {'linked': True, 'email': email}})
 
-def user_desync(user_id):
+async def user_desync(user_id):
     user_collection.update_one({'user_id': user_id}, {'$set': {'linked': False, 'email': ''}})
 
-def user_sync_from_web_app(user_info):
+async def user_sync_from_web_app(user_info):
     db_user = user_from_web_app.find_one({'user_id': user_info['tg_id']})
     if db_user:
         data = {
@@ -36,20 +36,20 @@ def user_sync_from_web_app(user_info):
             "refreshToken": user_info['refreshToken'],
         }
         user_from_web_app.insert_one(data)
-    user_sync(user_info['tg_id'], user_info['email'])
+    await user_sync(user_info['tg_id'], user_info['email'])
 
-def get_user_web_app(info):
+async def get_user_web_app(info):
     db_user = user_from_web_app.find_one({'user_id': info['tg_id']})
     if db_user:
         return {'accessToken': db_user['accessToken'],
                 'refreshToken': db_user['refreshToken'],
                 'tg_id': db_user['user_id']}
 
-def desync_from_web_app(info):
+async def desync_from_web_app(info):
     db_user = user_from_web_app.find_one({'user_id': info['tg_id']})
     if db_user:
         user_from_web_app.delete_one({'user_id': info['tg_id']})
 
 
-def get_user_by_chat_id(chat_id):
+async def get_user_by_chat_id(chat_id):
     return user_collection.find_one({"chat.id": int(chat_id)})
