a9c4d471c6eb653a7121ae4b96a5bcf494237b2b
timatima <starsinc12@yandex.ru>
2024-07-22 17:03:32 +0400

    delete keyboard v2

--- handlers/http_handler.py
+++ handlers/http_handler.py
@@ -134,8 +134,8 @@ async def configure_and_send_message(bot, dispatcher: Dispatcher, chat_id, messa
     storage_key = StorageKey(user_id=user['user_id'], bot_id=int(bot.id), chat_id=chat_id)
     await dispatcher.storage.set_state(storage_key, state)
 
-    mess = await bot.send_message(chat_id=chat_id, text=message_text, reply_markup=ReplyKeyboardRemove())
-    await bot.edit_message_reply_markup(chat_id=chat_id, message_id=mess.message_id, reply_markup=keyboard)
+    await bot.send_message(chat_id=chat_id, text="...", reply_markup=ReplyKeyboardRemove())
+    await bot.send_message(chat_id=chat_id, text=message_text, reply_markup=keyboard)
 
 
 async def send_ratings(request):
